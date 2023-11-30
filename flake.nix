69c6450b35c2b2b07af4c75d3c1c69b3a89c14a2
Nicolas Dufour <nrdufour@gmail.com>
2023-11-30 18:46:26 -0500

    feat: switching to flakes

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Nicolas Dufour's flakes";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    #nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      calypso = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
