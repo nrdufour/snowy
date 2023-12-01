6f27720aac341c598791a0d2998e73de43f8d0a2
Nicolas Dufour <nrdufour@gmail.com>
2023-11-30 19:35:19 -0500

    fix: Removing old useless stuff

--- hosts/calypso/configuration.nix
+++ hosts/calypso/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -14,9 +10,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "calypso"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
@@ -28,10 +21,6 @@
       options = "--delete-older-than 2d";
     };
   };
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -212,12 +201,6 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  #hardware.nvidia.prime = {
-  #  # Make sure to use the correct Bus ID values for your system!
-  #  intelBusId = "PCI:0:2:0";
-  #  nvidiaBusId = "PCI:14:0:0";
-  #};
-
   virtualisation = {
     podman = {
       enable = true;
@@ -227,10 +210,6 @@
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
-      # For Nixos version > 22.11
-      #defaultNetwork.settings = {
-      #  dns_enabled = true;
-      #};
     };
     virtualbox = {
       host.enable = true;
@@ -241,32 +220,5 @@
   };
 
   users.extraGroups.vboxusers.members = [ "ndufour" ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
 
 }
