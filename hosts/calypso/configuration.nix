3e0956e207d679910cb178ba55134695bba87c77
Nicolas Dufour <nrdufour@gmail.com>
2023-11-30 20:39:38 -0500

    feat: separating podman as common

--- hosts/calypso/configuration.nix
+++ hosts/calypso/configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../../common
     ];
 
   # Bootloader.
@@ -202,15 +203,6 @@
   };
 
   virtualisation = {
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
     virtualbox = {
       host.enable = true;
       host.enableExtensionPack = true;
