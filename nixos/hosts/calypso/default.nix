d14e92d31a45394cd723276856d5240bbbf1678d
Nicolas Dufour <nrdufour@gmail.com>
2023-12-01 20:21:52 -0500

    feath: finally getting rid of the configuration.nix file

--- nixos/hosts/calypso/default.nix
+++ nixos/hosts/calypso/default.nix
@@ -1,9 +1,52 @@
 { config, pkgs, ... }: {
-    imports = [
-        ./configuration.nix
+    imports =
+        [ # Include the results of the hardware scan.
+            ./hardware-configuration.nix
+            ../../personalities/base
+            ../../personalities/development
+            ../../personalities/laptop
+        ];
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+        vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+        git
+        chezmoi
+        python3
+        wget
+        weechat
+        starship
+        lshw
+        discord
+        gnome3.gnome-tweaks
+        kubectl
+        vagrant
+        yt-dlp
+        vlc
+        gnumake
+        direnv
+        sops
+        openssl
+        (vscode-with-extensions.override {
+            vscodeExtensions = with vscode-extensions; [
+            ms-python.python
+            bbenoist.nix
+            ];
+        })
+        ytfzf
+        any-nix-shell
+        freetube
     ];
+
+    programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    };
+
     networking.hostName = "calypso";
-    
+
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     system.stateVersion = "23.11";
 }
