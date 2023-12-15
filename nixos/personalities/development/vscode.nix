92fe76d0e26eb0f5ad796f76f35f35a823d26cc7
Nicolas Dufour <nrdufour@gmail.com>
2023-12-15 12:22:30 -0500

    fix(vscode): Adding missing extension

--- nixos/personalities/development/vscode.nix
+++ nixos/personalities/development/vscode.nix
@@ -52,6 +52,54 @@
                     version = "0.4.3";
                     sha256 = "1877k35biy72sh0c5f890z0mh36sr2cvd3d1mmw9mb2mdfq6xyqk";
                 }
+                {
+                    name = "vscode-yaml";
+                    publisher = "redhat";
+                    version = "1.14.0";
+                    sha256 = "0pww9qndd2vsizsibjsvscz9fbfx8srrj67x4vhmwr581q674944";
+                }
+                {
+                    name = "go";
+                    publisher = "golang";
+                    version = "0.40.0";
+                    sha256 = "1k2zv49pam436cgvb970dc9n4xpdkd07lp8jllrsx5vnx7m2ml52";
+                }
+                {
+                    name = "vscode-kubernetes-tools";
+                    publisher = "ms-kubernetes-tools";
+                    version = "1.3.15";
+                    sha256 = "1x6npc90p6b1wx5sd1hd0x0djahmffr6lw9cxh2zg10rbpq48w8i";
+                }
+                {
+                    name = "terraform";
+                    publisher = "hashicorp";
+                    version = "2.29.0";
+                    sha256 = "tGBs7+5ho+mvcJQF5gi/XfoiJ70MuCtFFOiPi2baae8=";
+                }
+                {
+                    name = "path-autocomplete";
+                    publisher = "ionutvmi";
+                    version = "1.25.0";
+                    sha256 = "0jjqh3p456p1aafw1gl6xgxw4cqqzs3hssr74mdsmh77bjizcgcb";
+                }
+                {
+                    name = "vscode-pylance";
+                    publisher = "ms-python";
+                    version = "2023.11.10";
+                    sha256 = "1i2xywjw4dghw0bbgk2z90zhgiy7sxdl4yv88399shjwyvbp2psf";
+                }
+                {
+                    name = "vscode-docker";
+                    publisher = "ms-azuretools";
+                    version = "1.28.0";
+                    sha256 = "0nmc3pdgxpmr6k2ksdczkv9bbwszncfczik0xjympqnd2k0ra9h0";
+                }
+                {
+                    name = "remote-ssh";
+                    publisher = "ms-vscode-remote";
+                    version = "0.107.1";
+                    sha256 = "1q9xp8id9afhjx67zc7a61zb572f296apvdz305xd5v4brqd9xrf";
+                }
             ];
         })
     ];
