c8d526c291cfd8ab69efa98fa89c403b47294e16
Nicolas Dufour <nrdufour@gmail.com>
2024-01-25 10:01:08 -0500

    feat: adding nixos-hardware and the closest profile to the laptop

--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
   };
-  outputs = { self, nixpkgs, nixpkgs-unstable }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware }:
     let
     system = "x86_64-linux";
     overlay-unstable = final: prev: {
@@ -19,6 +20,7 @@
         modules = [
           # Overlays-module makes "pkgs.unstable" available in configuration.nix
           ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+          nixos-hardware.nixosModules.asus-rog-strix-g513im
           ./nixos/hosts/calypso
         ];
       };
