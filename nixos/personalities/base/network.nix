eb5554ab970cc0d446db16c25a69c99ea224ca62
Nicolas Dufour <nrdufour@gmail.com>
2024-03-30 07:51:36 -0400

    feat(network): turning on systemd.resolved service

--- nixos/personalities/base/network.nix
+++ nixos/personalities/base/network.nix
@@ -1,4 +1,11 @@
 {
     # Enable networking
     networking.networkmanager.enable = true;
+
+    # networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+
+    services.resolved = {
+        enable = true;
+        # fallbackDns = [ "9.9.9.9" "1.1.1.1" ];
+    };
 }
