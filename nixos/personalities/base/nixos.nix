42331bd91a19bb6b0e4a8e19928bb42c5b9798ee
Nicolas Dufour <nrdufour@gmail.com>
2024-04-10 07:59:48 -0400

    feat(cachix): adding a few cachix caches

--- nixos/personalities/base/nixos.nix
+++ nixos/personalities/base/nixos.nix
@@ -1,6 +1,21 @@
 { pkgs, ... }: {
     nix = {
         settings = {
+            trusted-substituters = [
+              "https://nix-community.cachix.org"
+              "https://numtide.cachix.org"
+              "https://nrdufour.cachix.org"
+            ];
+
+            trusted-public-keys = [
+              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+              "nrdufour.cachix.org-1:nwmZmZ3b4h4cEJtIRib7trk8SVB49trCnlxVSNz0YQs="
+            ];
+
+            # Fallback quickly if substituters are not available.
+            connect-timeout = 5;
+
             experimental-features = [ "nix-command" "flakes" ];
         };
         gc = {
