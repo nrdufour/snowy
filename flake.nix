f5a4a44305cae6dd46aeac1b7598ac50b20e4fd6
Nicolas Dufour <nrdufour@gmail.com>
2024-06-21 15:11:30 -0400

    fix: pinning hardware source because of conflict between unstable and 24.05 and update of the day

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:nixos/nixos-hardware";
+
+    ## Pining hardware for 24.05 for now
+    ## See https://github.com/NixOS/nixos-hardware/issues/996#issuecomment-2183266924 for explanations
+    nixos-hardware.url = "github:NixOS/nixos-hardware/2f893e185c850bcd6dbf4fbc0c61b1b90d23ff79";
   };
   outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware }:
     let
