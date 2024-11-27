841ec1ecb401b61b598b0333c2a43bb9072026ca
Nicolas Dufour <nrdufour@gmail.com>
2024-11-26 20:22:34 -0500

    fix: trouble with nvidia 56x and wayland/gnome 47

--- nixos/personalities/laptop/dm/gnome.nix
+++ nixos/personalities/laptop/dm/gnome.nix
@@ -19,4 +19,13 @@
         };
         geoclue2.enable = true;
     };
+
+    # Note: yet another trouble with nvidia, messing up with wayland and gnome 47
+    # see https://gitlab.freedesktop.org/mesa/mesa/-/issues/11723
+    # and https://gitlab.gnome.org/GNOME/gtk/-/issues/7010
+    # So we have to revert the GSK Renderer from "vulkan" to "ngl"
+    # --- Related to nvidia 56x
+    environment.variables = {
+        GSK_RENDERER = "ngl";
+    };
 }
