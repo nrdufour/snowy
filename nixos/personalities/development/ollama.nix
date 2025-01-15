93a303c8018e5240520afe9e14c6d410162825e2
Nicolas Dufour <nrdufour@gmail.com>
2025-01-15 07:41:17 -0500

    feat(ollama): switching to unstable version 0.5.4

--- nixos/personalities/development/ollama.nix
+++ nixos/personalities/development/ollama.nix
@@ -1,7 +1,12 @@
 {
+  pkgs,
+  ...
+}:
+{
   # From https://wiki.nixos.org/wiki/Ollama
   services.ollama = {
     enable = true;
+    package = pkgs.unstable.ollama; # 0.5.4
     acceleration = "cuda";
   };
 }
