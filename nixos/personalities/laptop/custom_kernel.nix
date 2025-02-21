17488a69b4a344fa1f5961b816cd5f8f2cedc1ac
Nicolas Dufour <nrdufour@gmail.com>
2025-02-20 19:20:23 -0500

    feat(kernel): no kvm == no go

--- nixos/personalities/laptop/custom_kernel.nix
+++ nixos/personalities/laptop/custom_kernel.nix
@@ -1,5 +1,7 @@
 { pkgs, ...}: {
     # Switch to kernel version 6.12.x
     # See https://nixos.wiki/wiki/Linux_kernel
-    boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_12;
+
+    ## Disable it for now: no kvm == no go !
+    # boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_12;
 }
