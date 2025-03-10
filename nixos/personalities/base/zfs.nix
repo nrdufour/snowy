433636dec67bc19319cf44a7c1551db05c230050
Nicolas Dufour <nrdufour@gmail.com>
2025-03-09 23:30:45 -0400

    fix(zfs): finally able to properly boot with zfs

--- nixos/personalities/base/zfs.nix
+++ nixos/personalities/base/zfs.nix
@@ -8,8 +8,16 @@
     ];
     zfs = {
       forceImportRoot = false; # if stuck on boot, modify grub options , force importing isn't secure
+      extraPools = [ "tank"];
+      devNodes = "/dev/disk/by-id";
     };
   };
+
+  # Disable this unit as it's slowing down the boot by at least 3 mins wait !
+  # See https://discourse.nixos.org/t/boot-faster-by-disabling-udev-settle-and-nm-wait-online/6339
+  # and main issue here https://github.com/NixOS/nixpkgs/issues/244562
+  # and upstream in zfs here https://github.com/openzfs/zfs/issues/10891
+  systemd.services.systemd-udev-settle.enable = false;
 
   services.zfs = {
     autoScrub.enable = true;
