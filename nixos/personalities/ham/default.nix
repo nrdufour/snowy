a17a0acc4094d4f16e7e5c9566886b20c8a63572
Nicolas Dufour <nrdufour@gmail.com>
2025-03-16 21:26:07 -0400

    feat(ham): adding sdrpp and gpredict for listening NOAA satellites

--- nixos/personalities/ham/default.nix
+++ nixos/personalities/ham/default.nix
@@ -6,5 +6,10 @@
     environment.systemPackages = with pkgs; [
         # Must have for FT8 and Olivia
         wsjtx
+
+        # For listening for NOAA satellites and others ;-)
+        ## See https://www.a-centauri.com/articoli/noaa-poes-satellites-reception for details
+        sdrpp # useful for anything actually
+        gpredict
     ];
 }
