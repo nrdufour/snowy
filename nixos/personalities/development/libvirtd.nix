b4d4a7544f2ea15f37f4a2b94c01f0d74004c340
Nicolas Dufour <nrdufour@gmail.com>
2025-07-02 10:45:48 -0400

    feat: preparing windows vm on kvm

--- nixos/personalities/development/libvirtd.nix
+++ nixos/personalities/development/libvirtd.nix
@@ -1,7 +1,18 @@
 {
+  pkgs,
+  ...
+}:
+{
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
 
     # Make sure any users are included to vboxusers
     users.users.ndufour.extraGroups = [ "libvirtd" ];
+
+    environment.systemPackages = with pkgs; [
+        # Needed to simulate a TPM for windows 11 ...
+        # See https://getlabsdone.com/how-to-install-windows-11-on-kvm/
+        # and https://getlabsdone.com/how-to-enable-tpm-and-secure-boot-on-kvm/
+        swtpm
+    ];
 }
