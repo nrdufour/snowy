ab2e13c41d7924ccc1763c1fb4387a004b45e174
Nicolas Dufour <nrdufour@gmail.com>
2025-08-25 20:07:24 -0400

    feat(ai): adding claude-code and opencode

--- nixos/personalities/development/ai.nix
+++ nixos/personalities/development/ai.nix
@@ -12,9 +12,15 @@
 
   # Ollama UI at port 3000
   services.nextjs-ollama-llm-ui.enable = true;
-
-  # Adding llama-cpp as is first to experiment
+  
   environment.systemPackages = with pkgs; [
+    # Adding llama-cpp as is first to experiment
     llama-cpp
+
+    # Claude-code became a must-have as well
+    unstable.claude-code
+
+    # the contender ;) specially for ollama
+    unstable.opencode
   ];
 }
